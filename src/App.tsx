15d2d2bafbbb3a73b2201b06d9688580e1b35ed4
njajkes <naikes.nikolait@yandex.ru>
2023-07-01 15:22:23 +0300

    feat: updated counter structure

--- src/App.tsx
+++ src/App.tsx
@@ -4,30 +4,33 @@ import { useState } from "react";
 const getCount = async () => {
   const data = await fetch(import.meta.env.VITE_BACKEND_URL + "counter")
     .then((v) => v.json())
-    .then((v) => v.count as number)
-    .catch(() => 0);
-  return data;
+    .catch(() => ({ current: 0, target: null }));
+  return data as { current: number; target: number | null };
 };
 
 function App() {
   const { data } = useQuery({
-    refetchInterval: 1000,
+    refetchInterval: 3000,
     refetchIntervalInBackground: true,
     queryFn: getCount,
     queryKey: ["sub_count"],
-    initialData: 0,
+    initialData: { current: 0, target: null },
   });
 
-  const to = +(new URLSearchParams(window.location.search).get("to") || 1);
+  const to = +(
+    new URLSearchParams(window.location.search).get("to") ||
+    data.target ||
+    1
+  );
 
   return (
     <div className="counter-container">
       <div
         className="counter-done"
-        style={{ right: 200 - (data / to) * 200 }}
+        style={{ right: 200 - (data.current / to) * 200 }}
       />
       <span className="counter">
-        {data} / {to}
+        {data.current} / {to}
       </span>
     </div>
   );
