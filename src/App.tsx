41caf3cf44190ca8c1763d728848cd811d9c8659
njajkes <naikes.nikolait@yandex.ru>
2023-07-03 08:58:18 +0300

    feat: setup router

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,13 @@
-import { useQuery } from "@tanstack/react-query";
-import { useState } from "react";
-
-const getCount = async () => {
-  const data = await fetch(import.meta.env.VITE_BACKEND_URL + "counter")
-    .then((v) => v.json())
-    .catch(() => ({ current: 0, target: null }));
-  return data as { current: number; target: number | null };
-};
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Counter } from "./counter";
 
 function App() {
-  const { data } = useQuery({
-    refetchInterval: 3000,
-    refetchIntervalInBackground: true,
-    queryFn: getCount,
-    queryKey: ["sub_count"],
-    initialData: { current: 0, target: null },
-  });
-
-  const to = +(
-    new URLSearchParams(window.location.search).get("to") ||
-    data.target ||
-    1
-  );
-
   return (
-    <div className="counter-container">
-      <div
-        className="counter-done"
-        style={{ right: 200 - (data.current / to) * 200 }}
-      />
-      <span className="counter">
-        {data.current} / {to}
-      </span>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="counter" element={<Counter />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
